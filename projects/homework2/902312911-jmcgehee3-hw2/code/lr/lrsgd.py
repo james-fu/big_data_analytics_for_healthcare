5143c30a38994e0dffc8e03ade55da4b5774344c
jlmcgehee21 <jlmcgehee21@gmail.com>
2017-02-13 17:49:18 -0500

    finished hw2, added hw3

---
+++ projects/homework2/902312911-jmcgehee3-hw2/code/lr/lrsgd.py
@@ -0,0 +1,39 @@
+# Do not use anything outside of the standard distribution of python
+# when implementing this class
+import math
+
+class LogisticRegressionSGD:
+    """
+    Logistic regression with stochastic gradient descent
+    """
+
+    def __init__(self, eta, mu, n_feature):
+        """
+        Initialization of model parameters
+        """
+        self.eta = eta
+        self.mu = mu
+        self.weight = [0.0] * n_feature
+
+    def fit(self, X, y):
+        """
+        Update model using a pair of training sample
+        """
+
+        yhat = self.predict_prob(X)
+        for i in X:
+            grad = y - (yhat * i[1])
+            self.weight[i[0]] = self.weight[i[0]] + (self.eta * grad - self.eta * self.mu * self.weight[i[0]])
+
+    def predict(self, X):
+        """
+        Predict 0 or 1 given X and the current weights in the model
+        """
+        return 1 if self.predict_prob(X) > 0.5 else 0
+
+    def predict_prob(self, X):
+        """
+        Sigmoid function
+        """
+
+        return 1.0 / (1.0 + math.exp(-math.fsum((self.weight[f]*v for f, v in X))))
---
+++ projects/homework3/code/src/main/scala/edu/gatech/cse8803/main/Main.scala
@@ -0,0 +1,173 @@
+/**
+ * @author Hang Su <hangsu@gatech.edu>.
+ */
+
+package edu.gatech.cse8803.main
+
+import java.text.SimpleDateFormat
+
+import edu.gatech.cse8803.clustering.{NMF, Metrics}
+import edu.gatech.cse8803.features.FeatureConstruction
+import edu.gatech.cse8803.ioutils.CSVUtils
+import edu.gatech.cse8803.model.{Diagnostic, LabResult, Medication}
+import edu.gatech.cse8803.phenotyping.T2dmPhenotype
+import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.clustering.{GaussianMixture, KMeans}
+import org.apache.spark.mllib.linalg.{DenseMatrix, Matrices, Vectors, Vector}
+import org.apache.spark.mllib.feature.StandardScaler
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.io.Source
+
+
+object Main {
+  def main(args: Array[String]) {
+    import org.apache.log4j.Logger
+    import org.apache.log4j.Level
+
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+    val sc = createContext
+    val sqlContext = new SQLContext(sc)
+
+    /** initialize loading of data */
+    val (medication, labResult, diagnostic) = loadRddRawData(sqlContext)
+    val (candidateMedication, candidateLab, candidateDiagnostic) = loadLocalRawData
+
+    /** conduct phenotyping */
+    val phenotypeLabel = T2dmPhenotype.transform(medication, labResult, diagnostic)
+
+    /** feature construction with all features */
+    val featureTuples = sc.union(
+      FeatureConstruction.constructDiagnosticFeatureTuple(diagnostic),
+      FeatureConstruction.constructLabFeatureTuple(labResult),
+      FeatureConstruction.constructMedicationFeatureTuple(medication)
+    )
+
+    val rawFeatures = FeatureConstruction.construct(sc, featureTuples)
+
+    val (kMeansPurity, gaussianMixturePurity, nmfPurity) = testClustering(phenotypeLabel, rawFeatures)
+    println(f"[All feature] purity of kMeans is: $kMeansPurity%.5f")
+    println(f"[All feature] purity of GMM is: $gaussianMixturePurity%.5f")
+    println(f"[All feature] purity of NMF is: $nmfPurity%.5f")
+
+    /** feature construction with filtered features */
+    val filteredFeatureTuples = sc.union(
+      FeatureConstruction.constructDiagnosticFeatureTuple(diagnostic, candidateDiagnostic),
+      FeatureConstruction.constructLabFeatureTuple(labResult, candidateLab),
+      FeatureConstruction.constructMedicationFeatureTuple(medication, candidateMedication)
+    )
+
+    val filteredRawFeatures = FeatureConstruction.construct(sc, filteredFeatureTuples)
+
+    val (kMeansPurity2, gaussianMixturePurity2, nmfPurity2) = testClustering(phenotypeLabel, filteredRawFeatures)
+    println(f"[Filtered feature] purity of kMeans is: $kMeansPurity2%.5f")
+    println(f"[Filtered feature] purity of GMM is: $gaussianMixturePurity2%.5f")
+    println(f"[Filtered feature] purity of NMF is: $nmfPurity2%.5f")
+    sc.stop 
+  }
+
+  def testClustering(phenotypeLabel: RDD[(String, Int)], rawFeatures:RDD[(String, Vector)]): (Double, Double, Double) = {
+    import org.apache.spark.mllib.linalg.Matrix
+    import org.apache.spark.mllib.linalg.distributed.RowMatrix
+
+    /** scale features */
+    val scaler = new StandardScaler(withMean = true, withStd = true).fit(rawFeatures.map(_._2))
+    val features = rawFeatures.map({ case (patientID, featureVector) => (patientID, scaler.transform(Vectors.dense(featureVector.toArray)))})
+    val rawFeatureVectors = features.map(_._2).cache()
+
+    /** reduce dimension */
+    val mat: RowMatrix = new RowMatrix(rawFeatureVectors)
+    val pc: Matrix = mat.computePrincipalComponents(10) // Principal components are stored in a local dense matrix.
+    val featureVectors = mat.multiply(pc).rows
+
+    val densePc = Matrices.dense(pc.numRows, pc.numCols, pc.toArray).asInstanceOf[DenseMatrix]
+    /** transform a feature into its reduced dimension representation */
+    def transform(feature: Vector): Vector = {
+      Vectors.dense(Matrices.dense(1, feature.size, feature.toArray).multiply(densePc).toArray)
+    }
+
+    /** TODO: K Means Clustering using spark mllib
+      *  Train a k means model using the variabe featureVectors as input
+      *  Set maxIterations =20 and seed as 8803L
+      *  Assign each feature vector to a cluster(predicted Class)
+      *  Obtain an RDD[(Int, Int)] of the form (cluster number, RealClass)
+      *  Find Purity using that RDD as an input to Metrics.purity
+      *  Remove the placeholder below after your implementation
+      **/
+    val kMeansPurity = 0.0
+
+    /** TODO: GMMM Clustering using spark mllib
+      *  Train a Gaussian Mixture model using the variabe featureVectors as input
+      *  Set maxIterations =20 and seed as 8803L
+      *  Assign each feature vector to a cluster(predicted Class)
+      *  Obtain an RDD[(Int, Int)] of the form (cluster number, RealClass)
+      *  Find Purity using that RDD as an input to Metrics.purity
+      *  Remove the placeholder below after your implementation
+      **/
+    val gaussianMixturePurity = 0.0
+
+
+    /** NMF */
+    val rawFeaturesNonnegative = rawFeatures.map({ case (patientID, f)=> Vectors.dense(f.toArray.map(v=>Math.abs(v)))})
+    val (w, _) = NMF.run(new RowMatrix(rawFeaturesNonnegative), 3, 100)
+    // for each row (patient) in W matrix, the index with the max value should be assigned as its cluster type
+    val assignments = w.rows.map(_.toArray.zipWithIndex.maxBy(_._1)._2)
+    // zip patientIDs with their corresponding cluster assignments
+    // Note that map doesn't change the order of rows
+    val assignmentsWithPatientIds=features.map({case (patientId,f)=>patientId}).zip(assignments) 
+    // join your cluster assignments and phenotypeLabel on the patientID and obtain a RDD[(Int,Int)]
+    // which is a RDD of (clusterNumber, phenotypeLabel) pairs 
+    val nmfClusterAssignmentAndLabel = assignmentsWithPatientIds.join(phenotypeLabel).map({case (patientID,value)=>value})
+    // Obtain purity value
+    val nmfPurity = Metrics.purity(nmfClusterAssignmentAndLabel)
+
+    (kMeansPurity, gaussianMixturePurity, nmfPurity)
+  }
+
+  /**
+   * load the sets of string for filtering of medication
+   * lab result and diagnostics
+    *
+    * @return
+   */
+  def loadLocalRawData: (Set[String], Set[String], Set[String]) = {
+    val candidateMedication = Source.fromFile("data/med_filter.txt").getLines().map(_.toLowerCase).toSet[String]
+    val candidateLab = Source.fromFile("data/lab_filter.txt").getLines().map(_.toLowerCase).toSet[String]
+    val candidateDiagnostic = Source.fromFile("data/icd9_filter.txt").getLines().map(_.toLowerCase).toSet[String]
+    (candidateMedication, candidateLab, candidateDiagnostic)
+  }
+
+  def loadRddRawData(sqlContext: SQLContext): (RDD[Medication], RDD[LabResult], RDD[Diagnostic]) = {
+    /** You may need to use this date format. */
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX")
+
+    /** load data using Spark SQL into three RDDs and return them
+      * Hint: You can utilize edu.gatech.cse8803.ioutils.CSVUtils and SQLContext.
+      *
+      * Notes:Refer to model/models.scala for the shape of Medication, LabResult, Diagnostic data type.
+      *       Be careful when you deal with String and numbers in String type.
+      *       Ignore lab results with missing (empty or NaN) values when these are read in.
+      *       For dates, use Date_Resulted for labResults and Order_Date for medication.
+      * */
+
+    /** TODO: implement your own code here and remove existing placeholder code below */
+    val medication: RDD[Medication] =  sqlContext.sparkContext.emptyRDD
+    val labResult: RDD[LabResult] =  sqlContext.sparkContext.emptyRDD
+    val diagnostic: RDD[Diagnostic] =  sqlContext.sparkContext.emptyRDD
+
+    (medication, labResult, diagnostic)
+  }
+
+  def createContext(appName: String, masterUrl: String): SparkContext = {
+    val conf = new SparkConf().setAppName(appName).setMaster(masterUrl)
+    new SparkContext(conf)
+  }
+
+  def createContext(appName: String): SparkContext = createContext(appName, "local")
+
+  def createContext: SparkContext = createContext("CSE 8803 Homework Two Application", "local")
+}
