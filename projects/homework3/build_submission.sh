66f4e778a826734d8c33d0d7d940237a0fc44ab6
jlmcgehee21 <jlmcgehee21@gmail.com>
2017-02-27 08:08:20 -0500

    submitted hw3

--- projects/homework3/build_submission.sh
+++ projects/homework3/build_submission.sh
@@ -1,30 +1,25 @@
 #!/usr/bin/env bash
 
-dirname='902312911-jmcgehee3-hw2'
+dirname='902312911-jmcgehee3-hw3'
 
 
 rm -r $dirname
 rm -r $dirname.tar.gz
 
 mkdir $dirname
-mkdir $dirname/code
-mkdir $dirname/code/hive
-mkdir $dirname/code/lr
-mkdir $dirname/code/pig
-mkdir $dirname/code/zeppelin
+#mkdir $dirname/code
+#mkdir $dirname/code/hive
+#mkdir $dirname/code/lr
+#mkdir $dirname/code/pig
+#mkdir $dirname/code/zeppelin
 
-cp code/hive/event_statistics.hql $dirname/code/hive/event_statistics.hql
 
-cp code/lr/lrsgd.py $dirname/code/lr/lrsgd.py
-cp code/lr/mapper.py $dirname/code/lr/mapper.py
-cp code/lr/testensemble.py $dirname/code/lr/testensemble.py
 
-cp code/pig/etl.pig $dirname/code/pig/etl.pig
-cp code/pig/utils.py $dirname/code/pig/utils.py
-
-cp code/zeppelin/bdh_hw2_zeppelin.json $dirname/code/zeppelin/bdh_hw2_zeppelin.json
-
-cp homework2answer.pdf $dirname/homework2answer.pdf
+cp -r code/src $dirname/src
+cp -r code/sbt $dirname/sbt
+cp -r code/project $dirname/project
+cp code/build.sbt $dirname/build.sbt
+cp homework3answer.pdf $dirname/homework3answer.pdf
 
 tar -czvf $dirname.tar.gz $dirname
 
