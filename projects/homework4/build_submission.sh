c0be9acf300aef7382eadf938bfd8c72c7cf9e99
jlmcgehee21 <jlmcgehee21@gmail.com>
2017-03-01 19:01:05 -0500

    add build_submission for homework4

---
+++ projects/homework4/build_submission.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+dirname='902312911-jmcgehee3-hw4'
+
+
+rm -r $dirname
+rm -r $dirname.tar.gz
+
+mkdir $dirname
+
+cp -r code/src $dirname/src
+cp -r code/sbt $dirname/sbt
+cp -r code/project $dirname/project
+cp -r code/zeppelin $dirname/zeppelin
+cp code/build.sbt $dirname/build.sbt
+cp homework4answer.pdf $dirname/homework4answer.pdf
+
+tar -czvf $dirname.tar.gz $dirname
+
+rm -r $dirname
+
+
+
